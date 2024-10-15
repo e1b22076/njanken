cc26e0c290fa72e139219a1427293a54d59d2e40
e1b22076 <e1b22076@oit.ac.jp>
2024-10-15 18:41:46 +0900

    発展課題

--- src/main/java/oit/is/z2450/kaizi/njanken/controller/jankenController.java
+++ src/main/java/oit/is/z2450/kaizi/njanken/controller/jankenController.java
@@ -1,5 +1,7 @@
 package oit.is.z2450.kaizi.njanken.controller;
 
+import java.util.Random;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,6 +10,8 @@ import org.springframework.ui.Model;
 
 @Controller
 public class jankenController {
+
+  private static final String[] hands = { "Gu", "Choki", "Pa" };
 
   @PostMapping("/janken")
   public String enterRoom(@RequestParam String name, Model model) {
@@ -25,13 +29,17 @@ public class jankenController {
   public String goResult(@RequestParam String hand, Model model) {
     // 'name'というキーで、フォームから送られた値をモデルに追加
     model.addAttribute("hand", "あなたの手" + hand);
-    String opponentHand = "Gu"; // ここをランダムにすることも可能
+    Random random = new Random();
+
+    String opponentHand = hands[random.nextInt(hands.length)];
     model.addAttribute("opponentHand", "相手の手" + opponentHand);
 
     String result;
     if (hand.equals(opponentHand)) {
       result = "Draw";
-    } else if ((hand.equals("Pa") && opponentHand.equals("Gu"))) {
+    } else if ((hand.equals("Gu") && opponentHand.equals("Choki") ||
+        (hand.equals("Choki") && opponentHand.equals("Pa")) ||
+        (hand.equals("Pa") && opponentHand.equals("Gu")))) {
       result = "You Win!";
     } else {
       result = "You Lose";
