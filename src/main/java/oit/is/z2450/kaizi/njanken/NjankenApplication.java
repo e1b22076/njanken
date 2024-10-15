2dec9168b64827f748881e7a30a5ab7481cca7f4
e1b22076 <e1b22076@oit.ac.jp>
2024-10-15 17:01:34 +0900

    repositoryの初期化

---
+++ src/main/java/oit/is/z2450/kaizi/njanken/NjankenApplication.java
@@ -0,0 +1,13 @@
+package oit.is.z2450.kaizi.njanken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NjankenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NjankenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/z2450/kaizi/njanken/NjankenApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.z2450.kaizi.njanken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NjankenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
